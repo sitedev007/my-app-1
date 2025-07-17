d40a2ec3b44afbac68ca53f9d099eb8c6963e1bc
ajay-satbhadre <ajaysatbhadre@gmail.com>
2025-07-18 00:40:46 +0530

    test-changes

--- backend/start.sh
+++ backend/start.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
+echo "Script contents:"
+cat -v $MY_SCRIPT_FILE_PATH
+
+echo "Removing carriage returns from script file..."
+sed -i 's/\r$//' $MY_SCRIPT_FILE_PATH
+
 # run the script
 bash $MY_SCRIPT_FILE_PATH
-
-
-
-# cd /tmp
-
-# mkdir -p /tmp/.ssh
-
-# echo "$SSH_PRIVATE_KEY" > /tmp/.ssh/id_rsa
-
-# chmod 600 /tmp/.ssh/id_rsa
-
-# ssh-keyscan github.com >> /tmp/.ssh/known_hosts
-
-# git clone --depth 1 --filter=blob:none --sparse git@github.com:ajay-satbhadre/amazon-keyword-tracker.git
-
-# cd amazon-keyword-tracker && git sparse-checkout set backend && cd backend
-
-# cp -rp * /usr/app && cd /usr/app && cp -rp ~/lib/* .
-
-# echo "Files after copy: " && ls -la
-
-# npm start
 
