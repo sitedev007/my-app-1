3704e61c2151a8c91ea1b78d72cfc67b61006842
ajay-satbhadre <ajaysatbhadre@gmail.com>
2025-07-17 21:51:00 +0530

    Added files for the frontend

---
+++ frontend/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "$PRIVATE_SSH_KEY" > ~/.ssh/id_rsa
+
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
+
+chmod 600 ~/.ssh/id_rsa
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+git clone --depth 1 --filter=blob:none --sparse git@github.com:ajay-satbhadre/amazon-keyword-tracker.git /usr/app
+cd /usr/app && git sparse-checkout set frontend
+
+cd frontend && cp -rp ~/modules/* . && npm start
+
