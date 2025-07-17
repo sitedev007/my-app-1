821b12ed5becbe2acdf74d1622d4517917b9bdf7
ajay-satbhadre <ajaysatbhadre@gmail.com>
2025-07-18 01:11:50 +0530

    test-permission

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+
+echo "Checking permissions for /usr/script..."
+ls -lda /usr/script
+
+echo "installing dos2unix..."
+apt-get install dos2unix
+
 echo "Script contents:"
 cat -v $MY_SCRIPT_FILE_PATH
 
 echo "Removing carriage returns from script file..."
-sed -i 's/\r$//' $MY_SCRIPT_FILE_PATH
+dos2unix $MY_SCRIPT_FILE_PATH
 
 # run the script
 bash $MY_SCRIPT_FILE_PATH
